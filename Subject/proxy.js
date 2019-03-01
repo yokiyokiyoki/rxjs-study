dfe141b744e4bfa625b6472d32f496826cdc998f
youjie <youjie@datastory.com.cn>
2019-03-01 16:48:06 +0800

    cdn的下载下来放在本地，不然网速极慢

--- Subject/proxy.js
+++ Subject/proxy.js
@@ -1,2 +1,22 @@
-const { Observable } = rxjs;
-console.log(Observable);
+const { Observable, interval, subject } = rxjs;
+
+console.log(Observable, interval, subject);
+const interval$ = interval(1000);
+
+/**
+ * 两个观察者获得的是不一样事件
+ */
+interval$.subscribe(v => {
+  console.log(`${v}`);
+});
+
+setTimeout(() => {
+  interval$.subscribe(v => {
+    console.log(`${v}+setTimeout`);
+  });
+}, 2000);
+
+/**
+ * 两个观察者获得的是相同事件
+ * subject桥接代理
+ */
