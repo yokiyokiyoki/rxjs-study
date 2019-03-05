5d5b8e00742a6b594c304c33b4bb7407ae17ced5
youjie <youjie@datastory.com.cn>
2019-03-05 11:16:22 +0800

    添加注释

--- Subject/proxy.js
+++ Subject/proxy.js
@@ -6,20 +6,26 @@ const interval$ = interval(1000);
 /**
  * 两个观察者获得的是不一样事件
  */
-interval$.subscribe(v => {
-  console.log(`${v}`);
-});
+// interval$.subscribe(v => {
+//   console.log(`${v}`);
+// });
 
-setTimeout(() => {
-  interval$.subscribe(v => {
-    console.log(`${v}+setTimeout`);
-  });
-}, 2000);
+// setTimeout(() => {
+//   interval$.subscribe(v => {
+//     console.log(`${v}+setTimeout`);
+//   });
+// }, 2000);
 
 /**
  * 两个观察者获得的是相同事件
  * subject桥接代理
  */
 const subject = new Subject();
-interval$.subscribe(subject);
 //subject正在观察interval$
+interval$.subscribe(subject);
+
+subject.subscribe(val => console.log(`First observer ${val}`));
+//这里拿到的val是相同的执行，即使这里延迟了，这就是区别
+setTimeout(() => {
+  subject.subscribe(val => console.log(`Second observer ${val}`));
+}, 2000);
